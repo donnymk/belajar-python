a148c0c85142aa443343b32e5943e2699b0f95bc
Donny Malik Kurniawan <bossdony@gmail.com>
2023-01-20 22:22:36 +0700

    for loop, while loop, continue

---
+++ 21. while_loop.py
@@ -0,0 +1,11 @@
+# Program while loop
+
+# sintax
+# while kondisi:
+#     perintah
+
+data = ""
+
+while data != "x":
+    print("masuk perulangan")
+    data = input("data: ")
---
+++ 22. continue.py
@@ -0,0 +1,8 @@
+# Continue
+
+# contoh program untuk mencetak angka ganjil
+# angka genap akan diabaikan dengan keyword continue
+for i in range(1, 100):
+    if i % 2 == 1:
+        continue
+    print(i)
